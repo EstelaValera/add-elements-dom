1514066b1707e7f07a5c474e691b1f1331fb858c
Estela <estela.valera96@gmail.com>
2024-06-11 20:34:43 +0200

    Ejercicio Terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
- // Aquí tu código
+document.getElementById("agregar").addEventListener("click", function() {
+    var elemento = prompt("Ingrese el elemento:");
+    if (elemento) {
+        var lista = document.getElementById("lista");
+        var nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = elemento;
+        lista.appendChild(nuevoElemento);
+    } else {
+        alert("Por favor ingrese un elemento válido.");
+    }
+});
